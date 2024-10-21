e7b41301cf9ab653da25b28f9d08252fe98b05ce
Suzy Kim <skme1103@naver.com>
2024-10-21 17:43:17 +0900

    Fix error polyfill

--- dist/index.js
+++ dist/index.js
@@ -1,12 +1,15 @@
-'use strict';
+"use strict";
 
+var _interopRequireDefault = require("@babel/runtime-corejs3/helpers/interopRequireDefault");
+var _promise = _interopRequireDefault(require("@babel/runtime-corejs3/core-js-stable/promise"));
+var _setTimeout2 = _interopRequireDefault(require("@babel/runtime-corejs3/core-js-stable/set-timeout"));
 var arrowFunction = function arrowFunction() {
-    return 'Hello, Babel!';
+  return 'Hello, Babel!';
 };
-var promiseExample = new Promise(function (resolve, reject) {
-    setTimeout(function () {
-        return resolve('Promise resolved!');
-    }, 1000);
+var promiseExample = new _promise["default"](function (resolve, reject) {
+  (0, _setTimeout2["default"])(function () {
+    return resolve('Promise resolved!');
+  }, 1000);
 });
 promiseExample.then(console.log);
 console.log(arrowFunction());
