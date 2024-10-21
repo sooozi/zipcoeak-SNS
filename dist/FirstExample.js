c93236dbc2b5551ba3cb39866339a694866efd4a
Suzy Kim <skme1103@naver.com>
2024-10-21 17:49:53 +0900

    transe JSX

---
+++ dist/FirstExample.js
@@ -0,0 +1,7 @@
+'use strict';
+
+var _interopRequireDefault = require('@babel/runtime-corejs3/helpers/interopRequireDefault');
+var _react = _interopRequireDefault(require('react'));
+function FirstExample() {
+    return /*#__PURE__*/ _react.default.createElement('h1', null, 'Hello, World');
+}
--- dist/index.js
+++ dist/index.js
@@ -2,14 +2,11 @@
 
 var _interopRequireDefault = require("@babel/runtime-corejs3/helpers/interopRequireDefault");
 var _promise = _interopRequireDefault(require("@babel/runtime-corejs3/core-js-stable/promise"));
-var _setTimeout2 = _interopRequireDefault(require("@babel/runtime-corejs3/core-js-stable/set-timeout"));
-var arrowFunction = function arrowFunction() {
+const arrowFunction = () => {
   return 'Hello, Babel!';
 };
-var promiseExample = new _promise["default"](function (resolve, reject) {
-  (0, _setTimeout2["default"])(function () {
-    return resolve('Promise resolved!');
-  }, 1000);
+const promiseExample = new _promise.default((resolve, reject) => {
+  setTimeout(() => resolve('Promise resolved!'), 1000);
 });
 promiseExample.then(console.log);
 console.log(arrowFunction());
