553455710c6c3db953317f63453f89b1ece1fefd
Suzy Kim <skme1103@naver.com>
2024-11-04 16:50:04 +0900

    Check error

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,4 @@
 
 // console.log('Total:', sum('1', 3));
 
-import sum from './sum';
-
-console.log('Total:', sum(1, 3));
+console.log('Hello, TypeScript!');
--- webpack.config.cjs
+++ webpack.config.cjs
@@ -1,49 +1,45 @@
 // Generated using webpack-cli https://github.com/webpack/webpack-cli
 
-import HtmlWebpackPlugin from 'html-webpack-plugin';
-import { resolve as _resolve } from 'path';
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
 
 const isProduction = process.env.NODE_ENV == 'production';
 
 const config = {
     entry: './src/index.ts',
     output: {
-        path: _resolve(__dirname, 'dist'),
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js', // 번들 파일 이름 추가
     },
     devServer: {
         open: true,
         host: 'localhost',
+        port: 3000, // 포트 번호 추가 (필요에 따라 수정 가능)
     },
     plugins: [
         new HtmlWebpackPlugin({
             template: 'index.html',
         }),
-
-        // Add your plugins here
-        // Learn more about plugins from https://webpack.js.org/configuration/plugins/
     ],
     module: {
         rules: [
             {
                 test: /\.(ts|tsx)$/i,
                 loader: 'ts-loader',
-                exclude: ['/node_modules/'],
+                exclude: /node_modules/,
             },
             {
                 test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
                 type: 'asset',
             },
-
-            // Add your rules for custom modules here
-            // Learn more about loaders from https://webpack.js.org/loaders/
         ],
     },
     resolve: {
-        extensions: ['.tsx', '.ts', '.jsx', '.js', '...'],
+        extensions: ['.tsx', '.ts', '.jsx', '.js'],
     },
 };
 
-export default () => {
+module.exports = () => {
     if (isProduction) {
         config.mode = 'production';
     } else {
