c75d0a40d7d0c9aff0e7e55beb135e8ff8b15175
Suzy Kim <skme1103@naver.com>
2024-11-06 17:28:17 +0900

    Check error

---
+++ dist/App.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const App = () => {
+    return (hid
+        < /div>);
+};
+exports.default = App;
--- dist/index.js
+++ dist/index.js
@@ -1,9 +1,4 @@
 "use strict";
-
-var _interopRequireDefault = require("@babel/runtime-corejs3/helpers/interopRequireDefault");
-var _sum = _interopRequireDefault(require("./sum"));
 // import sum from './sum';
-
 // console.log('Total:', sum('1', 3));
-
-console.log('Total:', (0, _sum.default)(1, 3));
+console.log('Hello, TypeScript!');
