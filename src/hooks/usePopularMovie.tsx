9c9def968373a4abf2dc9af8e545a0093fc47c04
Suzy Kim <skme1103@naver.com>
2024-11-13 12:56:09 +0900

    Complete feature-movieList

--- src/hooks/usePopularMovie.tsx
+++ src/hooks/usePopularMovie.tsx
@@ -1,32 +1,16 @@
-// import { useQuery } from '@tanstack/react-query';
-// import api from '../utils/api';
-
-// const fetchPopularMovies = () => {
-//     // API 키를 URL 쿼리 파라미터로 전달
-//     return api.get(
-//         `/movie/popular?api_key=${import.meta.env.VITE_TMDB_API_KEY}`,
-//     );
-// };
-
-// export const usePopularMoviesQuery = () => {
-//     return useQuery({
-//         queryKey: ['movie-popular'],
-//         queryFn: fetchPopularMovies,
-//     });
-// };
-
 import { useQuery } from '@tanstack/react-query';
 import api from '../utils/api';
 
-const fetchPopularMovies = (page: number = 1) => {
+const fetchPopularMovies = () => {
+    // API 키를 URL 쿼리 파라미터로 전달
     return api.get(
-        `/movie/popular?api_key=${import.meta.env.VITE_TMDB_API_KEY}&page=${page}`,
+        `/movie/popular?api_key=${import.meta.env.VITE_TMDB_API_KEY}`,
     );
 };
 
-export const usePopularMoviesQuery = (page: number) => {
+export const usePopularMoviesQuery = () => {
     return useQuery({
-        queryKey: ['movie-popular', page], // 페이지 번호를 쿼리 키에 포함시켜 캐시 구분
-        queryFn: () => fetchPopularMovies(page), // 페이지 번호를 fetch 함수에 전달
+        queryKey: ['movie-popular'],
+        queryFn: fetchPopularMovies,
     });
 };
