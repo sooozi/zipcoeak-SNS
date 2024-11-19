fa04780de2c99e8058a4fc2ff7e6385ccd6ef6b8
Suzy Kim <skme1103@naver.com>
2024-11-19 16:31:39 +0900

    fix: run 에러 수정 중

--- webpack.config.mjs
+++ webpack.config.mjs
@@ -38,7 +38,7 @@ const config = {
     },
     plugins: [
         new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, 'index.html'), // 루트의 index.html을 명확히 지정
+            template: path.resolve(__dirname, 'public', 'index.html'),
         }),
         new webpack.DefinePlugin({
             'process.env.REACT_APP_API_KEY': JSON.stringify(
