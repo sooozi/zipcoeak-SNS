e38a0964424d9a7bfd2ec61855ba6f8c95e9528b
Suzy Kim <skme1103@naver.com>
2024-12-16 16:16:51 +0900

    add: 영화 디테일 페이지 제목, 영화 정보, 리뷰 노출 기능 추가

---
+++ src/queries/movieDetailId/useMovieDetailIdQuery.tsx
@@ -0,0 +1,16 @@
+import { useQuery } from '@tanstack/react-query';
+import api from '../../utils/api';
+
+// API에서 '현재 상영 중인 영화' 데이터를 가져오는 함수
+const fetchMovieDetail = (id: string) =>
+    api
+        .get(
+            `/movie/${id}?api_key=${process.env.REACT_APP_API_KEY}&language=ko-KR`,
+        )
+        .then(res => res.data); // 'res.data'를 반환하여 data를 바로 사용할 수 있게
+
+export const useMovieDetailQuery = (id: string) =>
+    useQuery({
+        queryKey: ['movie-detail'],
+        queryFn: () => fetchMovieDetail(id),
+    });
---
+++ src/queries/movieReview/useMovieReviewQuery.tsx
@@ -0,0 +1,15 @@
+import { useQuery } from '@tanstack/react-query';
+import api from '../../utils/api';
+
+const fetchMovieReview = (id: string) =>
+    api
+        .get(
+            `/movie/${id}/reviews?api_key=${process.env.REACT_APP_API_KEY}&language=ko-KR`,
+        )
+        .then(res => res.data); // 'res.data'를 반환하여 data를 바로 사용할 수 있게
+
+export const useMovieReviewQuery = (id: string) =>
+    useQuery({
+        queryKey: ['movie-review', id], //query key에 ID를 포함시켜 캐싱을 ID별로 관리
+        queryFn: () => fetchMovieReview(id),
+    });
